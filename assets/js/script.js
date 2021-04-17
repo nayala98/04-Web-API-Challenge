905c3b4fe49bd8d4aeb625ebb47bf74009b3d46b
nayala <nathaniel.ayala20@gmail.com>
2021-04-16 21:51:26 -0500

    Question One JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,38 @@
 //Main Page
 var mainEl = document.querySelector("#main");
 
-//Creating aspects of the STart Page| container, title, main paragraph, and quiz button
+//Function that creates question 1 and its answers
+var questionOne = function() {
+    var questionOneContainer = document.createElement("div");
+    questionOneContainer.className = "question-one-container";
+    mainEl.appendChild(questionOneContainer);
+
+    //question 1
+    var questionOne = document.createElement("h1");
+    questionOne.textContent = "Commonly Used data types DO NOT Include:";
+    questionOneContainer.appendChild(questionOne);
+
+    //Answers for question 1
+    var answerOne = document.createElement("button");
+    answerOne.textContent = "strings";
+    questionOneContainer.appendChild(answerOne);
+
+    var answerTwo = document.createElement("button");
+    answerTwo.textContent = "booleans";
+    questionOneContainer.appendChild(answerTwo);
+
+    var answerThree = document.createElement("button");
+    answerThree.textContent = "alerts";
+    questionOneContainer.appendChild(answerThree);
+
+    var answerFour = document.createElement("button");
+    answerFour.textContent = "numbers";
+    questionOneContainer.appendChild(answerFour);
+
+
+    console.log("Question One");
+}
+//Creating aspects of the Start Page| container, title, main paragraph, and quiz button
 var start = function() {
     var container = document.createElement("div");
     container.className = "home-container"
@@ -29,6 +60,7 @@ var start = function() {
         startQuizBtn.remove();
         homeHeader.remove();
         homeParagraph.remove();
+        questionOne();
     });
 }
 
