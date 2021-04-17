b80b377838183304e4d4e295ca07c27ef3e6afbc
nayala <nathaniel.ayala20@gmail.com>
2021-04-16 20:17:58 -0500

    Created structure of the main home page in js

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+//Main Page
+var main = document.querySelector("#main");
+
+//Creating aspects of the STart Page| container, title, main paragraph, and quiz button
+var start = function() {
+    var container = document.createElement("div");
+    container.className = "home-container"
+    main.appendChild(container);
+
+    var homeHeader = document.createElement("h1");
+    homeHeader.className = "home-title";
+    homeHeader.textContent = "Coding Quiz Challenge";
+    container.appendChild(homeHeader);
+
+    var homeParagraph = document.createElement("p");
+    homeParagraph.className = "home-text-p";
+    homeParagraph.textContent = "Try to answer the following code-related" +
+    " questions within the time limit. Keep in mind that incorrect answer will" +
+    " penalize your score/time by ten seconds!"
+    container.appendChild(homeParagraph);
+
+    var startQuizBtn = document.createElement("button");
+    startQuizBtn.className = "home-btn";
+    startQuizBtn.textContent = "Start Quiz";
+    container.appendChild(startQuizBtn);
+}
+start(); 
